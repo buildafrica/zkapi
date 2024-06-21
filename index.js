4a412843adea0d63c7594d9ee2ffd22614864da5
Andrew Miracle <me@andrewmiracle.com>
2024-06-21 13:39:59 +0000

    make port a variable from env

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+require('dotenv/config')
+
 const express = require('express');
+
 const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(4000, () => {
-  console.log('Listening on port 4000!');
+app.listen(process.env.PORT, () => {
+  console.log('Listening on port!' + process.env.PORT);
 });
 
 app.get('/', (req, res) => {
